e209ef7a4b4fe035406b2dcea530ed738248d137
riyaraj-web <your-riyaraj-web-riiyaraj079@gmail.com>
2025-08-17 10:37:59 +0530

    Resolved merge conflicts in App.jsx and ignored node_modules/package-lock.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,73 @@
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import ForgetPassword from './pages/ForgetPassword';
-import RecruiterDashboard from './pages/recruiter/Dashboard';
-import RecruiterJobs from './pages/recruiter/Jobs';
-import RecruiterJobDetails from './pages/recruiter/JobDetails';
-import RecruiterApplications from './pages/recruiter/Applications';
-import RecruiterInterviews from './pages/recruiter/Interviews';
-import RecruiterCandidateDetails from './pages/recruiter/CandidateDetails';
-import RecruiterSettings from './pages/recruiter/Settings';
-import UserDashboard from './pages/user/Dashboard';
-import UserJobs from './pages/user/Jobs';
-import UserApplications from './pages/user/Applications';
-import UserInterviews from './pages/user/Interviews';
-import UserProfile from './pages/user/Profile';
-import NotFound from './pages/NotFound';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgetPassword from "./pages/ForgetPassword";
+
+// Recruiter Pages
+import RecruiterDashboard from "./pages/recruiter/Dashboard";
+import RecruiterJobs from "./pages/recruiter/Jobs";
+import RecruiterJobDetails from "./pages/recruiter/JobDetails";
+import RecruiterApplications from "./pages/recruiter/Applications";
+import RecruiterInterviews from "./pages/recruiter/Interviews";
+import RecruiterCandidateDetails from "./pages/recruiter/CandidateDetails";
+import RecruiterSettings from "./pages/recruiter/Settings";
+
+// User Pages
+import UserDashboard from "./pages/user/Dashboard";
+import UserJobs from "./pages/user/Jobs";
+import UserApplications from "./pages/user/Applications";
+import UserInterviews from "./pages/user/Interviews";
+import UserProfile from "./pages/user/Profile";
+
+// 404
+import NotFound from "./pages/NotFound";
+
+// Context
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    {/* Public Routes */}
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<LoginPage />} />
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ForgetPassword />} />
 
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/reset-password" element={<ForgetPassword />} />
+          {/* Recruiter Routes */}
+          <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
+          <Route path="/recruiter/jobs" element={<RecruiterJobs />} />
+          <Route path="/recruiter/jobs/:id" element={<RecruiterJobDetails />} />
+          <Route
+            path="/recruiter/applications"
+            element={<RecruiterApplications />}
+          />
+          <Route
+            path="/recruiter/interviews"
+            element={<RecruiterInterviews />}
+          />
+          <Route
+            path="/recruiter/candidates/:id"
+            element={<RecruiterCandidateDetails />}
+          />
+          <Route path="/recruiter/settings" element={<RecruiterSettings />} />
 
-                    {/* Recruiter Routes */}
-                    <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
-                    <Route path="/recruiter/jobs" element={<RecruiterJobs />} />
-                    <Route path="/recruiter/jobs/:id" element={<RecruiterJobDetails />} />
-                    <Route path="/recruiter/applications" element={<RecruiterApplications />} />
-                    <Route path="/recruiter/interviews" element={<RecruiterInterviews />} />
-                    <Route path="/recruiter/candidates/:id" element={<RecruiterCandidateDetails />} />
-                    <Route path="/recruiter/settings" element={<RecruiterSettings />} />
+          {/* User Routes */}
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+          <Route path="/user/jobs" element={<UserJobs />} />
+          <Route path="/user/applications" element={<UserApplications />} />
+          <Route path="/user/interviews" element={<UserInterviews />} />
+          <Route path="/user/profile" element={<UserProfile />} />
 
-                    {/* User Routes */}
-                    <Route path="/user/dashboard" element={<UserDashboard />} />
-                    <Route path="/user/jobs" element={<UserJobs />} />
-                    <Route path="/user/applications" element={<UserApplications />} />
-                    <Route path="/user/interviews" element={<UserInterviews />} />
-                    <Route path="/user/profile" element={<UserProfile />} />
-
-                    {/* 404 Page */}
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
+          {/* Catch All - 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
